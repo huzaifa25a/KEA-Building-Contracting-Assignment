e321f913f6c1153c5682a048b48ecaf3ac224cff
Huzaifa25a <pachisahuzaifa@gmail.com>
2026-02-10 11:04:52 +0400

    Update PostgreSQL connection to use environment variables

--- server/db.js
+++ server/db.js
@@ -2,12 +2,20 @@ const pkg = require('pg');
 const {Pool} = pkg;
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'projects_tracker',
-    password: 'hp25a5253',
-    dialect: 'postgres',
-    port: 5432
-})
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
+  });
+  
+
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'projects_tracker',
+//     password: 'hp25a5253',
+//     dialect: 'postgres',
+//     port: 5432
+// })
 
 module.exports = pool;
